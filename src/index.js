a642641f74660f68b2952fb8b441967027f4869e
keithchasen <keithchasen89@gmail.com>
2022-04-10 01:15:32 +0300

    basic game object class

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext('2d');
 
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
 ctx.clearRect(0, 0, 800, 600);
 
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 100, 100);
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
-ctx.fillStyle = '#00f';
-ctx.fillRect(120, 150, 50, 50);
+paddle.draw(ctx);
